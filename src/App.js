b865f4580ccf077320e042958ff12399cfbb7d3c
Marouane <m.marouane14@gmail.com>
2022-07-05 12:53:08 +0100

    Template Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Listing from './pages/Listing';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Inner from './components/Inner';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <Header/>
+      <Inner/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Inner.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+const Inner = () => {
+
+    return (
+         <>
+            <main id="main">
+                <section id="breadcrumbs" className="breadcrumbs">
+                <div className="container-fluid">
+
+                    <div className="row justify-content-center">
+                    <div className="col-xl-10">
+                        <ol>
+                        <li><a href="index.html">Home</a></li>
+                        <li>Inner Page</li>
+                        </ol>
+                        <h2>Inner Page</h2>
+                    </div>
+                    </div>
+
+                </div>
+                </section>
+
+                <section className="inner-page">
+                    <div className="container-fluid">
+                        <div className="row justify-content-center">
+                        <div className="col-xl-10">
+                            <div className="row">
+                            <p>
+                                Example inner page template
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Inner;
