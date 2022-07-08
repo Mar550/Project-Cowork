8f38739f245d4ab657818e41493fd3b1b4d968ac
Marouane <m.marouane14@gmail.com>
2022-07-08 20:52:56 +0100

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Listing from './pages/Listing';
@@ -8,16 +10,40 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Inner from './components/Inner';
 import Contact from './pages/Contact';
-
+import Place from './pages/Place'
 
 function App() {
 
   return (
-    <div id="root">
+    <Router>
       <Header/>
-      <Contact/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}>
+          
+        </Route>
+        <Route path ='/about' element={<About/>}>
+        </Route>
+
+        <Route path ='/listing' element={<Listing/>}>
+        </Route>
+
+        <Route path ='/profile' element={<Profile/>}>
+        </Route>
+
+        <Route path ='/contact' element={<Contact/>}>
+        </Route>
+
+        <Route path ='/login' element={<Login/>}>
+        </Route>
+
+        <Route path ='/register' element={<Register/>}>
+        </Route>
+
+        <Route path ='/place' element={<Place/>}>
+        </Route>
+      </Routes>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,38 @@
 import React from "react";
-
+import './Login.css';
+import {FaGooglePlus} from 'react-icons/fa';
+import {AiFillTwitterCircle} from 'react-icons/ai';
+import {BsGithub} from 'react-icons/bs';
+import {FaFacebook} from 'react-icons/fa';
 
 const Login = () => {
     return (
         <>
-        
+        <section className="inner-page">
+                    <div className="container-fluid">
+                    <div class="background">
+                <div class="shape"></div>
+                <div class="shape"></div>
+            </div>
+            <form className="loginform">
+                <h3>Login </h3>
+
+                <label for="username">Username</label>
+                <input className="logininput" type="text" placeholder="Email or Phone" id="username"/>
+
+                <label for="password">Password</label>
+                <input className="logininput" type="password" placeholder="Password" id="password"/>
+
+                <button>Log In</button>
+                <div class="social">
+                <div class="go"> <FaGooglePlus/> </div>
+                <div class="gi"> <BsGithub/> </div>
+                <div class="tw"> <AiFillTwitterCircle/> </div>
+                <div class="fb"> <FaFacebook/> </div>
+                </div>
+            </form>
+            </div>
+        </section>  
         </>
     )
 }
